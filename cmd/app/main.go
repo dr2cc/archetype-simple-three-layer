3faf172b48077dade783bc55d00df564d9b0ff1c
Constantine <tmail99@gmail.com>
2025-12-02 14:19:15 +0300

    godotenv loads environment variables from the .env file

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,10 +4,17 @@ import (
 	"app/internal/config"
 	"app/internal/server"
 	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	// 1️⃣ Configuration
+	// загружает переменные окружения из файла .env (в корне)
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err)
+	}
+
 	cfg, err := config.NewConfig()
 	if err != nil {
 		log.Fatalf("Config error: %s", err)
